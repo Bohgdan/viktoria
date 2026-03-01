10726564c999ab0c1bc2aea7a22eab34175811da
unknown <bohdan.mar@gmail.com>
2026-03-01 14:56:58 +0100

    Fix logo: white filter instead of blend mode

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -74,7 +74,7 @@ export function Header() {
                   width={490}
                   height={200}
                   className="h-10 md:h-12 w-auto object-contain"
-                  style={{ mixBlendMode: 'screen' }}
+                  style={{ filter: 'brightness(0) invert(1)' }}
                   priority
                 />
               </Link>
